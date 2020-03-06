0ab2bd57a53eb4ff3890c2400cd55695d6280a7d
Petr Bouchal <pbouchal@gmail.com>
2020-03-06 10:52:27 +0100

    rstudio project template - basic

---
+++ R/rstudio_project_bindings.R
@@ -0,0 +1,15 @@
+
+#' FUNCTION_TITLE
+#'
+#' FUNCTION_DESCRIPTION
+#'
+#' @param  DESCRIPTION.
+#'
+#' @return RETURN_DESCRIPTION
+#' @examples
+#' # ADD_EXAMPLES_HERE
+#' @export
+schola_project <- function(path, ...) {
+  dir.create(path, recursive = TRUE, showWarnings = FALSE)
+  return(TRUE)
+}
