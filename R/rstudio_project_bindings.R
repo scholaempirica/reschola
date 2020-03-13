818da2d65db144f7851f845af5362bba685d24dc
Petr Bouchal <pbouchal@gmail.com>
2020-03-13 18:19:04 +0100

    improve git selection in project setup, also fixes #4

--- R/rstudio_project_bindings.R
+++ R/rstudio_project_bindings.R
@@ -9,35 +9,41 @@
 #' @examples
 #' # ADD_EXAMPLES_HERE
 schola_project <- function(path, ...) {
+  # print(usethis::proj_sitrep())
   dots <- list(...)
 
-  # create directort
+  orig_dir <- usethis::proj_sitrep()[['active_rstudio_proj']]
 
-  dir.create(path, recursive = TRUE, showWarnings = FALSE)
+  # create directory and project
 
-  # create project
-
+  usethis::create_project(path, open = FALSE)
+  usethis::proj_set(path)
   setwd(path)
-  usethis::create_project(".", open = FALSE)
+  # print("dir created")
+  # print(usethis::proj_sitrep())
 
   # handle git(hub) setup
 
+  # print("start switch")
   switch(dots[["vcs"]],
     none = usethis::ui_info("Not setting up any git version control."),
     "local git" = usethis::use_git(),
-    "private Github (mine)" = {
+    "Github: private owned by me" = {
+      # print("use_git")
       usethis::use_git()
+      # print("use_github")
       usethis::use_github(private = T)
     },
-    "public Github (mine)" = {
+    "Github: public owned by me" = {
       usethis::use_git()
       usethis::use_github()
     },
-    "public Github (scholaempirica)" = {
+    "Github: public owned by scholaempirica" = {
       usethis::use_git()
       usethis::use_github(organisation = "scholaempirica")
     },
   )
+
 
   if (dots[["copy_logos"]]) {
     logos <- reschola_file(
@@ -48,9 +54,12 @@ schola_project <- function(path, ...) {
     fs::dir_copy(logos, new_path = "logos")
   }
 
-
+  # print(usethis::proj_sitrep())
   draft_word(name = "First-Schola-styled-Word.Rmd", open = F)
   draft_redoc(name = "First-Schola-styled-redoc.Rmd", open = F)
 
+  # usethis::ui_todo("You should run {usethis::ui_code('usethis::proj_set(getwd())')} in your original session to get your working directory sorted.")
+  suppressMessages(usethis::proj_set(orig_dir))
+  setwd(orig_dir)
   return(TRUE)
 }
