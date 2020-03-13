80dd9cbc1dbced2e5db8dc88e29e1bc71c023310
Petr Bouchal <pbouchal@gmail.com>
2020-03-13 11:24:29 +0100

    parameterise + enrich project template

--- R/rstudio_project_bindings.R
+++ R/rstudio_project_bindings.R
@@ -8,8 +8,49 @@
 #' @return RETURN_DESCRIPTION
 #' @examples
 #' # ADD_EXAMPLES_HERE
-#' @export
 schola_project <- function(path, ...) {
+  dots <- list(...)
+
+  # create directort
+
   dir.create(path, recursive = TRUE, showWarnings = FALSE)
+
+  # create project
+
+  setwd(path)
+  usethis::create_project(".", open = FALSE)
+
+  # handle git(hub) setup
+
+  switch(dots[["vcs"]],
+    none = usethis::ui_info("Not setting up any git version control."),
+    "local git" = usethis::use_git(),
+    "private Github (mine)" = {
+      usethis::use_git()
+      usethis::use_github(private = T)
+    },
+    "public Github (mine)" = {
+      usethis::use_git()
+      usethis::use_github()
+    },
+    "public Github (scholaempirica)" = {
+      usethis::use_git()
+      usethis::use_github(organisation = "scholaempirica")
+    },
+  )
+
+  if (dots[["copy_logos"]]) {
+    logos <- reschola_file(
+      "rstudio", "templates", "project",
+      "schola_project_resources", "logos"
+    )
+
+    fs::dir_copy(logos, new_path = "logos")
+  }
+
+
+  create_word(name = "First-Schola-styled-Word.Rmd", open = F)
+  create_redoc(name = "First-Schola-styled-redoc.Rmd", open = F)
+
   return(TRUE)
 }
