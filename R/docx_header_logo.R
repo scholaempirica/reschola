7a6954c5e24c6b1a43fb8dae34b7c70e15502cd4
Petr Bouchal <pbouchal@gmail.com>
2020-03-19 13:24:49 +0100

    fix missing param in documentation

--- R/docx_header_logo.R
+++ R/docx_header_logo.R
@@ -8,6 +8,8 @@
 #' @param docx_path The Word document in which to replace logos. Must contain the bookmarks
 #'   `schola_logo` and `client_logo` in the header (files created from reschola templates do by default.)
 #' @param png_logo_path a PNG file which will be added/used as replacement
+#' @param action whether to add new logo on the right (`"add_client"`) or
+#'  replace default Schola logo on the left (`"replace_schola"`)
 #' @param height height of the new logo in the resulting document, in `cm`.
 #' By default, uses the height of an existing document
 #'
@@ -17,8 +19,10 @@
 #' @examples
 #' \dontrun{
 #' library(reschola)
-#' manage_docx_header_logos("draft.docx", png_logo_path = "logos/newlogo.png", action = "add_client")
-#' manage_docx_header_logos("draft.docx", png_logo_path = "logos/newlogo.png", action = "replace_schola")
+#' manage_docx_header_logos("draft.docx", png_logo_path = "logos/newlogo.png",
+#'                          action = "add_client")
+#' manage_docx_header_logos("draft.docx", png_logo_path = "logos/newlogo.png",
+#'                          action = "replace_schola")
 #' }
 manage_docx_header_logos <- function(docx_path, png_logo_path,
                             action = c("add_client", "replace_schola"),
