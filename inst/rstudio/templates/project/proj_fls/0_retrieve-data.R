e9bcd9828ef934cf2f7aaca180a37112a18411d4
Petr Bouchal <pbouchal@gmail.com>
2020-03-23 00:36:19 +0100

    document more in comments how template works

--- inst/rstudio/templates/project/proj_fls/0_retrieve-data.R
+++ inst/rstudio/templates/project/proj_fls/0_retrieve-data.R
@@ -1,2 +1,9 @@
+library(reschola)
 source("shared.R")
+
+# download all data from GDrive folder set in `shared.R` into `data-input`
 reschola::gd_download_folder(gd_url, overwrite = F, files_from_subfolders = T)
+
+# If there is other data you expect to only retrieve once
+# (from the web, public databases or APIs, etc.),
+# this might be a good place to store the code that does it.
