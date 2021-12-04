880823a50ace35278ee457ffa443f6144d2f332b
Jan Netik <netikja@gmail.com>
2021-12-04 15:53:26 +0100

    font registration tests fixed

--- tests/testthat/test-fonts.R
+++ tests/testthat/test-fonts.R
@@ -1,12 +1,14 @@
-test_that("font registration respect platform", {
+test_that("fonts register on Windows", {
+  skip_on_os(c("mac", "linux", "solaris"))
+
+  expect_true(register_reschola_fonts())
+  expect_message(register_reschola_fonts(), "[R|r]egistering fonts")
   expect_invisible(register_reschola_fonts())
+  expect_true(all(c("Roboto", "Roboto Condensed") %in% windowsFonts()))
+})
 
-  if (.Platform$OS.type == "windows") {
-    expect_equal(register_reschola_fonts(), TRUE)
-    expect_message(register_reschola_fonts(), "Registering fonts")
-  }
-
-  if (.Platform$OS.type != "windows") {
-    expect_equal(register_reschola_fonts(), FALSE)
-  }
+test_that("font registration skips on Unix", {
+  skip_on_os("windows")
+  expect_false(register_reschola_fonts())
+  expect_invisible(register_reschola_fonts())
 })
