bf688c7a5bf87ac54d3c29271bfeb22d79cc569d
Jan Netík <netikja@gmail.com>
2023-02-05 17:01:25 +0100

    fix ls attributes test to pick a survey with attributes

--- tests/testthat/test-ls_attributes.R
+++ tests/testthat/test-ls_attributes.R
@@ -1,7 +1,14 @@
 test_that("ls_get_attrs() returns a named character vector", {
   cur_surveys <- ls_surveys()
-  sample_survey <- cur_surveys[[nrow(cur_surveys), "survey_id"]]
-  attrs <- ls_get_attrs(sample_survey)
+  attrs <- NULL
+  newest_survey_index <- nrow(cur_surveys) + 1L
+
+  # if the newest survey has no attributes, try the next one until success
+  while (is.null(attrs)) {
+    newest_survey_index <- newest_survey_index - 1L
+    sample_survey <- cur_surveys[[newest_survey_index, "survey_id"]]
+    attrs <- ls_get_attrs(sample_survey)
+  }
 
   expect_type(attrs, "character")
   expect_named(attrs)
